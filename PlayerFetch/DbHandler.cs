96b0efa991b8dbf2b160e6ffb87e9807ba23ea17
Drew Berge <demlemon@hotmail.com>
2017-10-13 22:53:34 +0200

    added methods for loading scores

--- PlayerFetch/DbHandler.cs
+++ PlayerFetch/DbHandler.cs
@@ -51,5 +51,48 @@ namespace PlayerFetch
 
 		public static int loadAndStorePage(int pageNumber) =>
 			storeFetchPlayers(Page.loadPage(pageNumber));
+
+	    public static int loadAndStoreScores(int user_id)
+	    {
+		    var scoreList = Score.loadScores(user_id);
+
+		    db.SaveChanges();
+
+		    foreach (var score in scoreList)
+		    {
+				
+			    if (db.Players.Any(p => p.user_id == score.user_id))
+			    {
+					if (db.Scores.Any(s => s.user_id == score.user_id &&
+											s.beatmap_id == score.beatmap_id &&
+											s.enabled_mods == score.enabled_mods))
+					{
+					    db.Update(score);
+				    }
+				    else
+				    {
+					    db.Add(score);
+				    }
+			    }
+		    }
+
+		    return db.SaveChanges();
+	    }
+
+	    public static int loadAndStoreScores(Player player) =>
+		    loadAndStoreScores((int) player.user_id);
+
+	    public static int loadAllPlayerScores()
+	    {
+		    var total = 0;
+		    foreach (var player in db.Players)
+		    {
+				int addition = loadAndStoreScores(player);
+			    total += addition;
+				Console.WriteLine("{0} scores loaded for {1}", addition, player.username);
+		    }
+
+		    return total;
+	    }
     }
 }
