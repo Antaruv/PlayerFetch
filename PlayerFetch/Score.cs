df8d6e7392bea838c8e5d3e75f3a7fe52115ddeb
Drew Berge <demlemon@hotmail.com>
2017-10-13 22:51:40 +0200

    made score usable

--- PlayerFetch/Score.cs
+++ PlayerFetch/Score.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace PlayerFetch
 {
-	class Score
+	public class Score
 	{
 		public uint beatmap_id { get; set; }
 		public uint score { get; set; }
@@ -15,11 +22,29 @@ namespace PlayerFetch
 		public uint countmiss { get; set; }
 		public uint countkatu { get; set; }
 		public uint countgeki { get; set; }
-		public bool perfect { get; set; }
-		public int enabled_mods { get; set; }
-		public uint user_id { get; set; }
+		public uint perfect { get; set; }
+		public Mods enabled_mods { get; set; }
 		public string date { get; set; }
-		public uint rank { get; set; }
+		public string rank { get; set; }
 		public float pp { get; set; }
+
+		public uint user_id { get; set; }
+		public Player player { get; set; }
+
+		private static WebClient loader = new WebClient();
+
+		public static List<Score> loadScores(int user_id, int number)
+		{
+			string key = Environment.GetEnvironmentVariable("APIKEY");
+			string apiURL = "https://osu.ppy.sh/api/get_user_best?m=0&limit=" + number + "&k=" + key + "&u=" + user_id;
+
+			string json = loader.DownloadString(apiURL);
+			var scores = JsonConvert.DeserializeObject<Score[]>(json);
+
+			return scores.ToList();
+		}
+
+		public static List<Score> loadScores(int user_id) =>
+			loadScores(user_id, 100);
 	}
 }
